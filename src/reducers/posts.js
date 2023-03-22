43129c82059b422f8633206c3a40a44604691c91
vaansuit <vvansuit@outlook.com>
2023-03-22 16:12:39 -0300

    feat: added reducers to fetch and create

---
+++ src/reducers/posts.js
@@ -0,0 +1,14 @@
+//especificando o que fazer de acordo com o tipo de ação selecionada, posts inicia com uma array vazia porque em reducers eles sempre precisam ter um valor inicial
+
+const reducer = (posts = [], action) => {
+    switch (action.type) {
+        
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default: 
+            return posts;
+
+    }
+}
