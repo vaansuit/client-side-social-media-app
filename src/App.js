92bb90bf1b6ce7632138c6779665553991b89770
vaansuit <vvansuit@outlook.com>
2023-03-22 16:40:28 -0300

    feat: added getPosts action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import imageSocial from './assets/image1.jpg'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 import useStyles from './styles';
-
+import getPosts from './actions/posts';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, []);
 
     return (
        <Container maxWidth='lg'>
