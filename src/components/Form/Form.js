2f5a065f67341d96a2e0c64e391971dc05a27212
vaansuit <vvansuit@outlook.com>
2023-03-22 17:39:28 -0300

    fix: syntax adjust  in Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -23,9 +23,9 @@ const Form = () => {
     }
     return(
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant='h6'>Poste seu prato</Typography>
-                <TextField name='creator'  variant='outlined'  label='Creator' fullWidth  onChange={(e) => setPostData({ ...postData, creator: e.target.value })} value={postData.creator}/>
+                <TextField name='creator'  variant='outlined'  label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name='title'  variant='outlined'  label='Title' fullWidth  onChange={(e) => setPostData({ ...postData, title: e.target.value })} value={postData.title}/>
                 <TextField name='message'  variant='outlined'  label='Message' fullWidth  onChange={(e) => setPostData({ ...postData, message: e.target.value })} value={postData.message}/>
                 <TextField name='tags'  variant='outlined'  label='Tags' fullWidth  onChange={(e) => setPostData({ ...postData, tags: e.target.value })} value={postData.tags}/>
