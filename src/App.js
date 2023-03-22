5741942e1f5e5d4853ac1ee0101a99a4f88df70e
vaansuit <vvansuit@outlook.com>
2023-03-22 15:58:53 -0300

    feat: created api folder to fetch posts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import imageSocial from './assets/image1.jpg'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
+import useStyles from './styles';
+
 
 const App = () => {
+
+    const classes = useStyles();
+
     return (
        <Container maxWidth='lg'>
-            <AppBar position="static" color="inheridt">
-                <Typography variant="h2" align="center">Dead Poets Society</Typography>
-                <img src={imageSocial} alt='imageSocial' heigth="10"/>
+            <AppBar className={classes.appBar} position="static" color="inheridt">
+                <Typography className={classes.heading} variant="h2" align="center">Dead Poets Society</Typography>
+                <img className={classes.image} src={imageSocial} alt='imageSocial' height="80"/>
             </AppBar>
 
             <Grow in>
